b588568bee8c3a78b19415e6e27cfc62da43e45c
gabrielomn <gabriel.nobrega@ccc.ufcg.edu.br>
2019-03-25 19:06:40 -0300

    subindo metodos recursivos

--- rr03/src/main/java/recursao/MetodosRecursivos.java
+++ rr03/src/main/java/recursao/MetodosRecursivos.java
@@ -1,67 +1,67 @@
 package recursao;
 
+import java.util.Arrays;
+
 public class MetodosRecursivos {
 
 	public int calcularSomaArray(int[] array){
 		int result = 0;
-		// TODO ESCREVA AQUI O CÓDIGO (USANDO RECURSAO) PARA CALCULAR A SOMA
-		// DOS EMENTOS DE UM ARRAY
-		return result;
+		result += array[array.length - 1];
+
+		return result + calcularSomaArray(Arrays.copyOfRange(array,0, array.length - 2));
 	}
 	public long calcularFatorial(int n) {
-		long result = 1;
-		// TODO ESCREVA AQUI O CÓDIGO (USANDO RECURSAO) PARA CALCULAR E IMPRIMIR
-		// O FATORIAL DO PARAMETRO
-		// DE ACORDO COM O QUE FOI MOSTRADO NO EXERCCICIO. OBSERVE QUE SENDO O
-		// METODO
-		// RECURSIVO, O FATORIAL DOS NUMEROS ANTERIORES TAMBEM VAO SER IMPRESSOS
-		return result;
+		if(n == 1){
+			return 1;
+		}else{
+			return n*calcularFatorial(n-1);
+		}
 	}
 
 	public int calcularFibonacci(int n) {
-		int result = 1;
-		// TODO ESCREVA AQUI O CÓDIGO (USANDO RECURSAO) PARA CALCULAR E IMPRIMIR
-		// O N-ESIMO TERMO
-		// DA SEQUENCIA DE FIBONACCI, QUE TEM A SEGUINTE LEI DE FORMACAO: O
-		// PRIMEIRO E SEGUNDO NUMEROS
-		// DA SEQUENCIA SÃO 1. A PARTIR DO TERCEIRO TERMO, CADA TERMO DA
-		// SEQUENCIA É DADO
-		// PELA SOMA DOS OUTROS DOIS ANTERIORES. OBSERVE QUE SENDO O METODO
-		// RECURSIVO, O FIBONACCI DOS NUMEROS ANTERIORES TAMBEM VAO SER
-		// IMPRESSOS
-		return result;
+		if(n == 1 || n == 2){
+			return 1;
+		}else{
+			return calcularFibonacci(n -1) + calcularFibonacci( n -2);
+		}
 	}
 
 	public int countNotNull(Object[] array) {
-		int result = 0;
-		// TODO IMPLEMENTE AQUI O CODIGO QUE CONTA (USANDO RECURSAO) A
-		// QUANTIDADE DE ELEMENTOS NAO NULOS
-		// DE UM ARRAY DE OBJETOS RECEBIDO COMO PARAMETRO
-		return result;
+		if (array.length == 0){
+			return 0;
+		}else{
+			if (array[array.length - 1] == null){
+				return 1 + countNotNull(Arrays.copyOfRange(array, 0, array.length -2));
+			}else{
+				return countNotNull(Arrays.copyOfRange(array, 0, array.length -2));
+			}
+		}
 	}
 
 	public long potenciaDe2(int expoente) {
-		int result = 1;
-		// TODO IMPLEMENTE (USANDO RECURSAO) O CODIGO PARA PRODUZIR A N-ESIMA
-		// POTENCIA
-		// DE 2
-		return result;
+		if (expoente == 0){
+			return 1;
+		}else{
+			return 2*potenciaDe2(expoente - 1);
+		}
+
 	}
 
 	public double progressaoAritmetica(double termoInicial, double razao, int n) {
-		double result = 0;
-		// TODO IMPLEMENTE SEU CODIGO (USANDO RECURSAO) DE ENCONTRAR O n-ESIMO
-		// TERMO
-		// DA PROGRESSAO ARITMETICA, DADO O TERMO INICIAL E A RAZAO
-		return result;
+		if(n == 0){
+			return 0;
+		}else{
+			return progressaoAritmetica(termoInicial + razao, razao, n -1);
+		}
 	}
 
 	public double progressaoGeometrica(double termoInicial, double razao, int n) {
-		double result = 1;
-		// TODO IMPLEMENTE SEU CODIGO (USANDO RECURSAO) DE ENCONTRAR O n-ESIMO
-		// TERMO
-		// DA PROGRESSAO GEOMETRICA, DADO O TERMO INICIAL E A RAZAO
-		return result;
+		if(n == 0){
+			return 1;
+		}
+		else{
+			return progressaoGeometrica(termoInicial * razao, razao, n-1);
+		}
 	}
 	
 	
